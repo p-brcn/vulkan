ea4e532ba2e688542984e863a8c292b4e9ffb1c0
brcn <kristian381@hotmail.com>
2023-09-22 11:23:15 +0200

    Triangle pipeline stages

--- src/tve_pipeline.hpp
+++ src/tve_pipeline.hpp
@@ -7,14 +7,27 @@
 
 namespace tve {
 
-  struct PipelineConfigInfo{};
+  struct PipelineConfigInfo{
+    VkViewport viewport;
+    VkRect2D scissor;
+    VkPipelineViewportStateCreateInfo viewportInfo;
+    VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
+    VkPipelineRasterizationStateCreateInfo rasterizationInfo;
+    VkPipelineMultisampleStateCreateInfo multisampleInfo;
+    VkPipelineColorBlendAttachmentState colorBlendAttachment;
+    VkPipelineColorBlendStateCreateInfo colorBlendInfo;
+    VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
+    VkPipelineLayout pipelineLayout = nullptr;
+    VkRenderPass renderPass = nullptr;
+    uint32_t subpass = 0;
+  };
 
   class TvePipeline {
 
   public:
     TvePipeline(TveDevice &device, const std::string& vertFilepath, const std::string& fragFilepath, const PipelineConfigInfo configInfo);
 
-    ~TvePipeline() {};
+    ~TvePipeline();
 
     TvePipeline(const TvePipeline&) = delete;
     void operator=(const TvePipeline&) = delete;
