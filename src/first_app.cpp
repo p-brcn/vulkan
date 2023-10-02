c23890fb6931572b075e1db81e4dc2c2011ece62
brcn <kristian381@hotmail.com>
2023-10-02 18:00:48 +0200

    Implemented push constants

--- src/first_app.cpp
+++ src/first_app.cpp
@@ -1,10 +1,19 @@
 #include "first_app.hpp"
 
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+
 #include <stdexcept>
 #include <array>
 #include <cassert>
 
 namespace tve {
+
+  struct SimplePushConstantData {
+    alignas(8) glm::vec2 offset;
+    alignas(16) glm::vec3 color;
+  };
 
   FirstApp::FirstApp() {
     loadModels();
@@ -37,12 +46,18 @@ namespace tve {
   }
 
   void FirstApp::createPipelineLayout() {
+
+    VkPushConstantRange pushConstantRange{};
+    pushConstantRange.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
+    pushConstantRange.offset = 0;
+    pushConstantRange.size = sizeof(SimplePushConstantData);
+
     VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
     pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
     pipelineLayoutInfo.setLayoutCount = 0;
     pipelineLayoutInfo.pSetLayouts = nullptr;
-    pipelineLayoutInfo.pushConstantRangeCount = 0;
-    pipelineLayoutInfo.pPushConstantRanges = nullptr;
+    pipelineLayoutInfo.pushConstantRangeCount = 1;
+    pipelineLayoutInfo.pPushConstantRanges = &pushConstantRange;
     if (vkCreatePipelineLayout(tveDevice.device(), &pipelineLayoutInfo, nullptr, &pipelineLayout) != VK_SUCCESS) {
       throw std::runtime_error("failed to create pipeline layout");
     }
@@ -71,6 +86,9 @@ namespace tve {
   }
 
   void FirstApp::recordCommandBuffer(int imageIndex) {
+    static int frame = 0;
+    frame = (frame + 1) % 1000;
+
     VkCommandBufferBeginInfo beginInfo{};
     beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
 
@@ -87,7 +105,7 @@ namespace tve {
     renderPassInfo.renderArea.extent = tveSwapChain->getSwapChainExtent();
 
     std::array<VkClearValue, 2> clearValues{};
-    clearValues[0].color = {0.1f, 0.1f, 0.1f, 1.0f};
+    clearValues[0].color = {0.01f, 0.01f, 0.01f, 1.0f};
     clearValues[1].depthStencil = {1.0f, 0};
     renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
     renderPassInfo.pClearValues = clearValues.data();
@@ -107,7 +125,14 @@ namespace tve {
 
     tvePipeline->bind (commandBuffers[imageIndex]);
     tveModel->bind(commandBuffers[imageIndex]);
-    tveModel->draw(commandBuffers[imageIndex]);
+    for (int j = 0; j < 4; j++) {
+      SimplePushConstantData push{};
+      push.offset = {-0.5f + frame * 0.002f, -0.4f + j * 0.25f};
+      push.color = {0.0f, 0.0f, 0.02f + 0.25f * j};
+
+      vkCmdPushConstants(commandBuffers[imageIndex], pipelineLayout, VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT, 0, sizeof(SimplePushConstantData), &push);
+      tveModel->draw(commandBuffers[imageIndex]);
+    }
 
     vkCmdEndRenderPass(commandBuffers[imageIndex]);
     if (vkEndCommandBuffer(commandBuffers[imageIndex]) != VK_SUCCESS) {
